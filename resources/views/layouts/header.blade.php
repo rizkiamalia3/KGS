d1fa531056ce274893ddd969c357d47871387110
unknown <rizki@gmail.com>
2020-06-25 19:26:32 +0700

    fix asset 2

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -11,13 +11,13 @@
      <meta name="author" content="Tooplate">
      <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
      <link rel="shortcut icon" type="image/x-icon" href="{{ URL::asset('/template/images/logo.png') }}"/>
-     <link rel="stylesheet" href="{{ URL::asset('/template/css/bootstrap.min.css' ) }}>
-     <link rel="stylesheet" href="{{ URL::asset('/template/css/font-awesome.min.css' ) }}>
-     <link rel="stylesheet" href="{{ URL::asset('/template/css/animate.css' ) }}>
-     <link rel="stylesheet" href="{{ URL::asset('/template/css/owl.carousel.css' ) }}>
-     <link rel="stylesheet" href="{{ URL::asset('/template/css/owl.theme.default.min.css' ) }}>
+     <link rel="stylesheet" href="{{ URL::asset('/template/css/bootstrap.min.css' ) }}">
+     <link rel="stylesheet" href="{{ URL::asset('/template/css/font-awesome.min.css' ) }}">
+     <link rel="stylesheet" href="{{ URL::asset('/template/css/animate.css' ) }}">
+     <link rel="stylesheet" href="{{ URL::asset('/template/css/owl.carousel.css' ) }}">
+     <link rel="stylesheet" href="{{ URL::asset('/template/css/owl.theme.default.min.css' ) }}">
 
      <!-- MAIN CSS -->
-     <link rel="stylesheet" href="{{ URL::asset('/template/css/tooplate-style.css' ) }}>
+     <link rel="stylesheet" href="{{ URL::asset('/template/css/tooplate-style.css' ) }}">
 
 </head>
