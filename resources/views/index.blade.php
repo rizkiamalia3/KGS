990334a1fa64c3a6c784c7747fda07b18bd78afe
unknown <wildprojr31@gmail.com>
2020-06-26 15:46:04 +0700

    last edit

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -2,15 +2,14 @@
 <!-- HOME -->
 <section id="home" class="slider" data-stellar-background-ratio="0.5">
             <div class="row">
-
-                <div class="owl-carousel owl-theme">
+                <div class="owl-carousel owl-theme ">
                     @foreach($sliders as $slider)
                     @if($slider == null)
-                    <div class="item img {{ $loop->first ? 'active' : '' }}">
+                    <div class="owl-item img {{ $loop->first ? 'active' : '' }}">
                         <img src="" alt="">
                     </div>
                     @else
-                    <div class="item img {{ $loop->first ? 'active' : '' }}">
+                    <div class="owl-item img {{ $loop->first ? 'active' : '' }}">
                         <img src="{{ URL::asset('/images/sliders/'.$slider->image)}}" alt="{{ $slider->image_id }}">
                     </div>
                     @endif
@@ -18,5 +17,5 @@
                 </div>
             </div>
         </div>
-    </section>
+</section>
 @include('layouts.footer')
